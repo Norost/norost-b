546deb44245346032cf0f4e800cbdaec8cf9e374
David Hoppenbrouwers <david@salt-inc.org>
2021-09-02 20:49:11 +0200

    Replace Makefile with scripts, move kernel design to subfolder
    GNU make is inserting implicit 'rm' rules that I can't stamp out. This
    is the first time it happens to me, it's stupid as fuck and I can't be
    bothered.

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -x
+
+rm -rf build/
---
+++ mkdoc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+set -x
+
+IN="kernel"
+
+IN_DIR="Design"
+OUT_DIR="build/doc/design"
+
+mkdir -p "$OUT_DIR"
+
+for i in "$IN"
+do
+	rst2html5.py "Design/$i/Index.rst" "$OUT_DIR/$i.html"
+done
