d7116040d9e6a84d7fed3f75c897d918ddd56e2b
David Hoppenbrouwers <david@salt-inc.org>
2021-09-28 20:21:59 +0200

    Implement basic IDT

---
+++ kernel/src/arch/amd64/idt.rs
@@ -0,0 +1,87 @@
+use core::convert::TryFrom;
+use core::mem;
+
+#[repr(C)]
+pub struct IDTEntry {
+	offset_low: u16,
+	selector: u16,
+	ist: u8,
+	type_attributes: u8,
+	offset_high: u16,
+	offset_higher: u32,
+	_unused_1: u32,
+}
+
+impl IDTEntry {
+	const ATTRIBUTE_GATETYPE_INTERRUPT: u8 = 0xe;
+	const ATTRIBUTE_GATETYPE_TRAP: u8 = 0xf;
+	const ATTRIBUTE_PRESENT: u8 = 0x80;
+
+	const EMPTY: Self = Self {
+		offset_low: 0,
+		selector: 0,
+		ist: 0,
+		type_attributes: 0,
+		offset_high: 0,
+		offset_higher: 0,
+		_unused_1: 0,
+	};
+
+	pub fn new(selector: u16, handler: fn(), is_trap: bool, ist: u8) -> Self {
+		assert!(ist < 8, "ist out of bounds");
+		let handler = handler as usize;
+		Self {
+			offset_low: (handler >> 0) as u16,
+			selector,
+			ist,
+			type_attributes: Self::ATTRIBUTE_PRESENT
+				| is_trap
+					.then(|| Self::ATTRIBUTE_GATETYPE_TRAP)
+					.unwrap_or(Self::ATTRIBUTE_GATETYPE_INTERRUPT),
+			offset_high: (handler >> 16) as u16,
+			offset_higher: (handler >> 32) as u32,
+			_unused_1: 0,
+		}
+	}
+}
+
+#[repr(C)]
+pub struct IDT<const L: usize> {
+	descriptors: [IDTEntry; L],
+}
+
+impl<const L: usize> IDT<L> {
+	pub const fn new() -> Self {
+		Self {
+			descriptors: [IDTEntry::EMPTY; L],
+		}
+	}
+
+	pub fn set(&mut self, index: usize, entry: IDTEntry) {
+		self.descriptors[index] = entry;
+	}
+}
+
+#[repr(C)]
+#[repr(packed)]
+pub struct IDTPointer {
+	limit: u16,
+	offset: u64,
+}
+
+impl IDTPointer {
+	pub fn new<const L: usize>(idt: &'static IDT<L>) -> Self {
+		unsafe {
+			Self {
+				limit: u16::try_from(mem::size_of_val(idt) - 1).unwrap(),
+				offset: idt as *const _ as u64,
+			}
+		}
+	}
+
+	pub fn activate(&self) {
+		unsafe {
+			asm!("lidt [{0}]", in(reg) self);
+		}
+	}
+}
--- kernel/src/arch/amd64/mod.rs
+++ kernel/src/arch/amd64/mod.rs
@@ -1,5 +1,6 @@
 pub mod asm;
 mod gdt;
+mod idt;
 mod multiboot;
 mod tss;
 pub mod r#virtual;
@@ -11,12 +12,24 @@ static mut GDT: MaybeUninit<gdt::GDT> = MaybeUninit::uninit();
 // TODO do we really need to keep this in memory forever?
 static mut GDT_PTR: MaybeUninit<gdt::GDTPointer> = MaybeUninit::uninit();
 
+static mut IDT: idt::IDT<256> = idt::IDT::new();
+static mut IDT_PTR: MaybeUninit<idt::IDTPointer> = MaybeUninit::uninit();
+
 pub unsafe fn init() {
 	GDT.write(gdt::GDT::new(&TSS));
 	GDT_PTR.write(gdt::GDTPointer::new(core::pin::Pin::new(
 		GDT.assume_init_ref(),
 	)));
 	GDT_PTR.assume_init_mut().activate();
+
+	IDT.set(8, idt::IDTEntry::new(1 * 8, || {
+		fatal!("Double fault!");
+		halt();
+	}, true, 0));
+	IDT_PTR.write(idt::IDTPointer::new(&IDT));
+	IDT_PTR.assume_init_ref().activate();
+
+	unsafe { asm!("ud2") }
 }
 
 pub fn halt() -> ! {
