0dbf56231db9385f5f462da0e57e5aedff4df2d7
David Hoppenbrouwers <david@salt-inc.org>
2022-04-21 01:21:41 +0200

    Implement GlobalAlloc::realloc

--- kernel/src/memory/heap/mod.rs
+++ kernel/src/memory/heap/mod.rs
@@ -27,6 +27,8 @@ mod default {
 			}
 		}
 
+		// No alloc_zeroed as there is nothing to optimize to it.
+
 		unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
 			let c = Page::min_pages_for_bytes(layout.size());
 			let mut base = unsafe { PPN::from_ptr(ptr.cast::<Page>()) };
@@ -38,6 +40,28 @@ mod default {
 				})
 			}
 			.unwrap();
+		}
+
+		unsafe fn realloc(&self, ptr: *mut u8, old_layout: Layout, new_size: usize) -> *mut u8 {
+			let old_c = NonZeroUsize::new(Page::min_pages_for_bytes(old_layout.size()))
+				.expect("old size must be greater than 0");
+			let new_c = NonZeroUsize::new(Page::min_pages_for_bytes(new_size))
+				.expect("new size must be greater than 0");
+			if old_c == new_c {
+				// No need to reallocate, as there is still space left.
+				ptr
+			} else {
+				// We need to reallocate.
+				let new_layout = Layout::from_size_align(new_size, old_layout.align()).unwrap();
+				unsafe {
+					let new_ptr = self.alloc(new_layout);
+					if new_ptr != ptr::null_mut() {
+						new_ptr.copy_from_nonoverlapping(ptr, old_layout.size());
+						self.dealloc(ptr, old_layout);
+					}
+					new_ptr
+				}
+			}
 		}
 	}
 }
