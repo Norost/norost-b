9d399b68d4e3d93a2a4cfd691b963dff5f24f49a
David Hoppenbrouwers <david@salt-inc.org>
2022-04-27 23:55:18 +0200

    Implement norostb_rt::table::Object

--- lib/rust/rt/src/table.rs
+++ lib/rust/rt/src/table.rs
@@ -1,21 +1,67 @@
-use core::mem::ManuallyDrop;
-
+use super::io;
+use core::mem::{ManuallyDrop, MaybeUninit};
 use norostb_kernel::syscall;
+
 pub use norostb_kernel::{
 	io::ObjectInfo,
 	syscall::{TableId, TableInfo},
 	Handle,
 };
 
+#[derive(Debug)]
 pub struct Object(Handle);
 
 impl Object {
+	#[inline]
+	pub fn open(table: TableId, path: &[u8]) -> io::Result<Self> {
+		// Find a unique ID
+		io::open(table, path).map(Self)
+	}
+
+	#[inline]
+	pub fn create(table: TableId, path: &[u8]) -> io::Result<Self> {
+		io::create(table, path).map(Self)
+	}
+
+	#[inline]
+	pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
+		io::read(self.0, buf)
+	}
+
+	#[inline]
+	pub fn read_uninit(&self, buf: &mut [MaybeUninit<u8>]) -> io::Result<usize> {
+		io::read_uninit(self.0, buf)
+	}
+
+	#[inline]
+	pub fn write(&self, buf: &[u8]) -> io::Result<usize> {
+		io::write(self.0, buf)
+	}
+
+	pub fn seek(&self, pos: io::SeekFrom) -> io::Result<u64> {
+		io::seek(self.0, pos)
+	}
+
+	pub fn duplicate(&self) -> io::Result<Self> {
+		io::duplicate(self.0).map(Self)
+	}
+
+	pub fn as_raw(&self) -> Handle {
+		self.0
+	}
+
 	pub fn into_raw(self) -> Handle {
 		ManuallyDrop::new(self).0
 	}
 
 	pub fn from_raw(handle: Handle) -> Self {
 		Self(handle)
+	}
+}
+
+impl Drop for Object {
+	fn drop(&mut self) {
+		io::close(self.0)
 	}
 }
 
