4cb9f90540a3c0e0e67b88459738341c657f35a5
David Hoppenbrouwers <david@salt-inc.org>
2022-07-24 14:16:19 +0200

    Add binary name in panic output

--- lib/rust/rt_default/src/lib.rs
+++ lib/rust/rt_default/src/lib.rs
@@ -6,12 +6,20 @@
 #[global_allocator]
 static ALLOC: rt_alloc::Allocator = rt_alloc::Allocator;
 
+fn name() -> &'static str {
+	rt::args::Args::new()
+		.next()
+		.and_then(|s| core::str::from_utf8(s).ok())
+		.unwrap_or("??")
+}
+
 #[alloc_error_handler]
 fn alloc_error(layout: core::alloc::Layout) -> ! {
 	let _ = rt::io::stderr().map(|o| {
 		writeln!(
 			o,
-			"allocation failed for size {}, alignment {}",
+			"{}: allocation failed for size {}, alignment {}",
+			name(),
 			layout.size(),
 			layout.align()
 		)
@@ -21,6 +29,6 @@ fn alloc_error(layout: core::alloc::Layout) -> ! {
 
 #[panic_handler]
 fn panic_handler(info: &core::panic::PanicInfo) -> ! {
-	let _ = rt::io::stderr().map(|o| writeln!(o, "{}", info));
+	let _ = rt::io::stderr().map(|o| writeln!(o, "{}: {}", name(), info));
 	rt::exit(128)
 }
